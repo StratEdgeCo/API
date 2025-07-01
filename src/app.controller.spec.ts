8f5731eb4c275b1978d58e5da826722c920c788e
mvjdev <hei.maria.625@gmail.com>
2025-07-01 15:37:44 +0300

    feat: test automatic deployment

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World Test !"', () => {
+      expect(appController.getHello()).toBe('Hello World Test!');
     });
   });
 });
